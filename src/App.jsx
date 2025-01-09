039c5bfa2249c5e39e2af3c2c41d4592079a2741
Yamansaini0405 <sainiyaman14@gmail.com>
2025-01-10 01:34:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Navbar from "./comonents/Navbar";
+import MockTest from "./comonents/MockTest";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <MockTest/>
+    </>
+  );
+}
+
+export default App;
